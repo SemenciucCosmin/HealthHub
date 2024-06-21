7087e5a8e1d53860c793f0739517e939d71fbbf1
Semenciuc <cosmin.semenciuc@garmin.com>
2024-06-21 14:02:11 +0300

    Update colors

--- app/src/main/java/com/example/healthhub/presentation/theme/Colors.kt
+++ app/src/main/java/com/example/healthhub/presentation/theme/Colors.kt
@@ -5,277 +5,38 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
 val HealthHubLightColorScheme = lightColorScheme(
-    /**
-     *  Status bar
-     *  Device card tint
-     *  Outlined buttons text
-     *  Bottom sheet tint
-     *  Alert Dialog text buttons
-     */
-    primary = Color(0xFFEE6633),
+    primary = Color(0xFF33AAFF),
+    onPrimary = Color(0xFFF0F0F0),
 
-    /**
-     *
-     */
-//    onPrimary = Color.Red,
-
-    /**
-     * Floating Action Button
-     */
-    primaryContainer = Color(0xFFFFCCBB),
-
-    /**
-     * Floating Action Button content
-     */
-    onPrimaryContainer = Color(0xFFEE6633),
-
-    /**
-     *
-     */
-//    inversePrimary = Color.Red,
-
-    /**
-     *
-     */
-//    secondary = Color.Red,
-
-    /**
-     *
-     */
-//    onSecondary = Color.Red,
-
-    /**
-     *  Pair devices button
-     *  Device action buttons
-     */
-    secondaryContainer = Color(0xFFFFCCBB),
-
-    /**
-     *  Pair devices button content
-     *  Device action buttons content
-     */
-    onSecondaryContainer = Color(0xFFEE6633),
-
-    /**
-     *
-     */
-//    tertiary = Color.Red,
-
-    /**
-     *
-     */
-//    onTertiary = Color.Red,
-
-    /**
-     *
-     */
-//    tertiaryContainer = Color.Red,
-
-    /**
-     *
-     */
-//    onTertiaryContainer = Color.Red,
-
-    /**
-     *  Entire screen background
-     *  Device item
-     */
     background = Color(0xFFE8E8E8),
-
-    /**
-     * Device overlay text
-     * Loading text
-     * Device item card text and leading icon - May not be wanted
-     * Content in modal bottom sheet - May not be wanted
-     */
     onBackground = Color(0xFF616161),
 
-    /**
-     *  Device item card
-     *  Modal bottom sheet
-     *  Alert dialog
-     */
     surface = Color(0xFFF0F0F0),
+    onSurface = Color(0xFF101010),
 
-    /**
-     *  Alert dialog title
-     */
-    onSurface = Color(0xFFEE6633),
-
-    /**
-     * Locked button not checked
-     * Background of text field
-     */
     surfaceVariant = Color(0xFFE1E4ED),
-
-    /**
-     *  Modal bottom sheet drag handle
-     *  Locked button not checked content
-     *  Text field content
-     */
     onSurfaceVariant = Color(0xFF474A52),
 
-    /**
-     *  Device card tint
-     *  Bottom sheet tint
-     */
-    surfaceTint = Color(0xFFEE6633),
+    surfaceTint = Color(0xFFF0F0F0),
 
-    /**
-     *
-     */
-//    inverseSurface = Color.Red,
-
-    /**
-     *
-     */
-//    inverseOnSurface = Color.Red,
-
-    /**
-     * Outline buttons
-     */
-    outline = Color(0xFFEE6633),
-
-    /**
-     * Divider
-     */
-    outlineVariant = Color(0xFFEE6633),
+    outline = Color(0xFF6C6C6C),
+    outlineVariant = Color(0xFFC3C3C3),
 )
 
 val HealthHubDarkColorScheme = darkColorScheme(
-    /**
-     *  Status bar
-     *  Device card tint
-     *  Outlined buttons text
-     *  Bottom sheet tint
-     *  Alert Dialog text buttons
-     */
-    primary = Color(0xFFFFCCBB),
+    primary = Color(0xFF44BFFF),
 
-    /**
-     *
-     */
-//    onPrimary = Color.Red,
-
-    /**
-     * Floating Action Button
-     */
-    primaryContainer = Color(0xFFEE6633),
-
-    /**
-     * Floating Action Button content
-     */
-    onPrimaryContainer = Color(0xFFFFCCBB),
-
-    /**
-     *
-     */
-//    inversePrimary = Color.Red,
-
-    /**
-     *
-     */
-//    secondary = Color.Red,
-
-    /**
-     *
-     */
-//    onSecondary = Color.Red,
-
-    /**
-     *  Pair devices button
-     *  Device action buttons
-     */
-    secondaryContainer = Color(0xFFEE6633),
-
-    /**
-     *  Pair devices button content
-     *  Device action buttons content
-     */
-    onSecondaryContainer = Color(0xFFFFCCBB),
-
-    /**
-     *
-     */
-//    tertiary = Color.Red,
-
-    /**
-     *
-     */
-//    onTertiary = Color.Red,
-
-    /**
-     *
-     */
-//    tertiaryContainer = Color.Red,
-
-    /**
-     *
-     */
-//    onTertiaryContainer = Color.Red,
-
-    /**
-     *  Entire screen background
-     *  Device item
-     */
     background = Color(0xFF171717),
-
-    /**
-     * Device overlay text
-     * Loading text
-     * Device item card text and leading icon - May not be wanted
-     * Content in modal bottom sheet - May not be wanted
-     */
     onBackground = Color(0xFF9E9E9E),
 
-    /**
-     *  Device item card
-     *  Modal bottom sheet
-     *  Alert dialog
-     */
     surface = Color(0xFF0F0F0F),
+    onSurface = Color(0xFFF0F0F0),
 
-    /**
-     *  Alert dialog title
-     */
-    onSurface = Color(0xFFFFCCBB),
-
-    /**
-     * Locked button not checked
-     * Background of text field
-     */
     surfaceVariant = Color(0xFF474A52),
-
-    /**
-     *  Modal bottom sheet drag handle
-     *  Locked button not checked content
-     *  Text field content
-     */
     onSurfaceVariant = Color(0xFFE1E4ED),
 
-    /**
-     *  Device card tint
-     *  Bottom sheet tint
-     */
-    surfaceTint = Color(0xFFFFCCBB),
+    surfaceTint = Color(0xFF0F0F0F),
 
-    /**
-     *
-     */
-//    inverseSurface = Color.Red,
-
-    /**
-     *
-     */
-//    inverseOnSurface = Color.Red,
-
-    /**
-     * Outline buttons
-     */
-    outline = Color(0xFFFFCCBB),
-
-    /**
-     * Divider
-     */
-    outlineVariant = Color(0xFFFFCCBB),
+    outline = Color(0xFF6C6C6C),
+    outlineVariant = Color(0xFF3B3B3B),
 )
